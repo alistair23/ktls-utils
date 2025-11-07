05918a40db6c04aaef44e1fdbf6485bc54a7e3ae
Alistair Francis <alistair.francis@wdc.com>
2025-11-07 21:34:33 +1000

    WIP: handshake: Set request_update when sending KeyUpdate
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- src/tlshd/handshake.c
+++ src/tlshd/handshake.c
@@ -316,8 +316,9 @@ void tlshd_service_socket(void)
 
 		switch (parms.key_update_type) {
 		case HANDSHAKE_KEY_UPDATE_TYPE_SEND:
-			/* We don't expect a KeyUpdate response */
-			if (gnutls_session_key_update(session, 0))
+			tlshd_log_debug("We do expect a KeyUpdate response");
+			/* We do expect a KeyUpdate response */
+			if (gnutls_session_key_update(session, GNUTLS_KU_PEER))
 				goto deinit;
 
 			parms.session_status = tlshd_initialize_ktls(session, WRITE);
@@ -326,6 +327,7 @@ void tlshd_service_socket(void)
 			/* We received a KeyUpdate and the peer doesn't
 			 * expect a response
 			 */
+			tlshd_log_debug("We received a KeyUpdate and the peer doesn't expect a response");
 			if (gnutls_handshake_update_receiving_key(session))
 				goto deinit;
 
@@ -335,6 +337,7 @@ void tlshd_service_socket(void)
 			/* We received a KeyUpdate and the peer does
 			 * expect a response
 			 */
+			tlshd_log_debug("We received a KeyUpdate and the peer does expect a response");
 			if (gnutls_handshake_update_receiving_key(session))
 				goto deinit;
 
@@ -393,8 +396,9 @@ void tlshd_service_socket(void)
 
 		switch (parms.key_update_type) {
 		case HANDSHAKE_KEY_UPDATE_TYPE_SEND:
-			/* We don't expect a KeyUpdate response */
-			if (gnutls_session_key_update(session, 0))
+			/* We do expect a KeyUpdate response */
+			tlshd_log_debug("We do expect a KeyUpdate response");
+			if (gnutls_session_key_update(session, GNUTLS_KU_PEER))
 				goto deinit;
 
 			parms.session_status = tlshd_initialize_ktls(session, WRITE);
@@ -403,6 +407,7 @@ void tlshd_service_socket(void)
 			/* We received a KeyUpdate and the peer doesn't
 			 * expect a response
 			 */
+			tlshd_log_debug("We received a KeyUpdate and the peer doesn't expect a response");
 			if (gnutls_handshake_update_receiving_key(session))
 				goto deinit;
 
@@ -412,6 +417,7 @@ void tlshd_service_socket(void)
 			/* We received a KeyUpdate and the peer does
 			 * expect a response
 			 */
+			tlshd_log_debug("We received a KeyUpdate and the peer does expect a response");
 			if (gnutls_handshake_update_receiving_key(session))
 				goto deinit;
 
